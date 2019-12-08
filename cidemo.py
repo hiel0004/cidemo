15de2ee236723bee2e2d6782ce6a01fdf83ff926
Thomas Hielsberg <hiel0004@umn.edu>
2019-12-08 13:19:13 -0800

    Add date 20191208.131913

--- cidemo.py
+++ cidemo.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
+''' This is a program displays text output'''
+
+
 print("hello inet4121")
+print('20191208.130641')
+print(Current time: '20191208.131818')
+print(Current time: '20191208.131913')
